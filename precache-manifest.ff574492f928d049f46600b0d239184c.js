c5ff493e98920eb3c92c616ce46d3fa13c8788c6
Alexandr <aleksandr.lonski.86@gmail.com>
2021-01-26 12:04:22 +0300

    Updates

---
+++ precache-manifest.ff574492f928d049f46600b0d239184c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f26157a3826a9005e017",
+    "url": "/alexandrlonski/game-of-thrones/static/css/main.43473269.chunk.css"
+  },
+  {
+    "revision": "f26157a3826a9005e017",
+    "url": "/alexandrlonski/game-of-thrones/static/js/main.f26157a3.chunk.js"
+  },
+  {
+    "revision": "8dcb33c5be29b26496be",
+    "url": "/alexandrlonski/game-of-thrones/static/css/1.adce926e.chunk.css"
+  },
+  {
+    "revision": "8dcb33c5be29b26496be",
+    "url": "/alexandrlonski/game-of-thrones/static/js/1.8dcb33c5.chunk.js"
+  },
+  {
+    "revision": "0c94ec527170bca0befb",
+    "url": "/alexandrlonski/game-of-thrones/static/js/runtime~main.0c94ec52.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "/alexandrlonski/game-of-thrones/static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/alexandrlonski/game-of-thrones/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "9cf7a9b314570fc005adf5b4bd8ed803",
+    "url": "/alexandrlonski/game-of-thrones/index.html"
+  }
+];
